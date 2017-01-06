40cbf8c8c8ad98a31fe97c4701fb90cf2a2205d8
Shailesh Gupta <shaileshgupta94@gmail.com>
2017-01-06 12:24:13 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root'); // root
-define('DB_PASSWORD', 'root');          // root
-define('DB_DATABASE', 'buysell'); //buysell
+define('DB_SERVER', '');
+define('DB_USERNAME', ''); // root
+define('DB_PASSWORD', '');          // root
+define('DB_DATABASE', ''); //buysell
 $connection = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 if ($connection->connect_errno) {
     die("ERROR : -> ".$DBcon->connect_error);
